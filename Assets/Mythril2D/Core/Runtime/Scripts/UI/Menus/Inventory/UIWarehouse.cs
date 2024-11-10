298edc8ac88f23022f625d549d02bb6819ab30fa
Atl212 <56058541+Atl212@users.noreply.github.com>
2024-11-10 16:28:44 +0900

    2024-11-10-16:28-Adjust Art Resources : FireFairy & SmallDemon & HugeDemon & UI Icon

--- Assets/Mythril2D/Core/Runtime/Scripts/UI/Menus/Inventory/UIWarehouse.cs
+++ Assets/Mythril2D/Core/Runtime/Scripts/UI/Menus/Inventory/UIWarehouse.cs
@@ -1,9 +1,15 @@
+using log4net.Core;
+using Palmmedia.ReportGenerator.Core.Parser.Analysis;
+using TMPro;
 using UnityEngine;
 
 namespace Gyvr.Mythril2D
 {
     public class UIWarehouse : MonoBehaviour, IUIMenu
     {
+        [Header("References")]
+        [SerializeField] private TextMeshProUGUI m_backpackMoney = null;
+        [SerializeField] private TextMeshProUGUI m_warehouseMoney = null;
         [SerializeField] private UIInventoryBag m_bag = null;
         [SerializeField] private UIWarehouseBag m_warehouse = null;
 
@@ -64,8 +70,14 @@ namespace Gyvr.Mythril2D
         // Message called by children using SendMessageUpward when the bag or equipment changed
         private void UpdateUI()
         {
+            UpdateInfoSection();
             m_bag.UpdateSlots();
             m_warehouse.UpdateSlots();
+        }
+        private void UpdateInfoSection()
+        {
+            m_backpackMoney.text = StringFormatter.Format("{0}", GameManager.InventorySystem.backpackMoney.ToString());
+            m_warehouseMoney.text = StringFormatter.Format("{0}", GameManager.WarehouseSystem.warehouseMoney.ToString());
         }
 
         private void OnItemClicked(Item item, EItemLocation location)
