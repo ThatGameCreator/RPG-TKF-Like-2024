60dbbfd7a7cbdb56bbbb28c3169a12817f3b5417
Atl212 <56058541+Atl212@users.noreply.github.com>
2024-12-11 19:58:54 +0900

    2024-12-11-20:00-Add Function and Dates
    Damage item scripts
    Converts some items into usable types
    Text for currency items, herbs, mushrooms, etc.

---
+++ Assets/Mythril2D/Core/Runtime/Scripts/Database/Items/DamageItem.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+namespace Gyvr.Mythril2D
+{
+    [CreateAssetMenu(menuName = AssetMenuIndexer.Mythril2D_Items + nameof(DamageItem))]
+    public class DamageItem : Item
+    {
+        [Header("Effect")]
+        [SerializeField] private int m_healthToDamage = 1;
+        [SerializeField] private int m_damageType = 1;
+
+        [Header("Audio")]
+        [SerializeField] private AudioClipResolver m_drinkAudio;
+
+        public override void Use(CharacterBase target, EItemLocation location)
+        {
+            if (GameManager.WarehouseSystem.isOpenning == true)
+            {
+                if (location == EItemLocation.Bag && GameManager.WarehouseSystem.IsWarehouseFull() == false)
+                {
+                    GameManager.InventorySystem.RemoveFromBag(this);
+                    GameManager.WarehouseSystem.AddToWarehouse(this);
+                }
+                else if (location == EItemLocation.Warehouse && GameManager.InventorySystem.IsBackpackFull(this) == false)
+                {
+                    GameManager.InventorySystem.AddToBag(this);
+                    GameManager.WarehouseSystem.RemoveFromWarehouse(this);
+                }
+            }
+            else
+            {
+                target.Damage(new DamageOutputDescriptor
+                {
+                    source = EDamageSource.Unknown,
+                    attacker = this,
+                    damage = m_healthToDamage,
+                    damageType = EDamageType.None,
+                    distanceType = EDistanceType.None,
+                    flags = EDamageFlag.None
+                });
+
+                GameManager.NotificationSystem.audioPlaybackRequested.Invoke(m_drinkAudio);
+                GameManager.InventorySystem.RemoveFromBag(this);
+            }
+        }
+    }
+}
---
+++ Assets/Mythril2D/Core/Runtime/Scripts/Database/Items/HealthItem.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Gyvr.Mythril2D
+{
+    [CreateAssetMenu(menuName = AssetMenuIndexer.Mythril2D_Items + nameof(HealthItem))]
+    public class HealthItem : Item
+    {
+        [Header("Effect")]
+        [SerializeField] private int m_healthToRestore = 1;
+
+        [Header("Audio")]
+        [SerializeField] private AudioClipResolver m_drinkAudio;
+
+        public override void Use(CharacterBase target, EItemLocation location)
+        {
+            if (GameManager.WarehouseSystem.isOpenning == true)
+            {
+                if (location == EItemLocation.Bag && GameManager.WarehouseSystem.IsWarehouseFull() == false)
+                {
+                    GameManager.InventorySystem.RemoveFromBag(this);
+                    GameManager.WarehouseSystem.AddToWarehouse(this);
+                }
+                else if (location == EItemLocation.Warehouse && GameManager.InventorySystem.IsBackpackFull(this) == false)
+                {
+                    GameManager.InventorySystem.AddToBag(this);
+                    GameManager.WarehouseSystem.RemoveFromWarehouse(this);
+                }
+            }
+            else
+            {
+                if (target.currentStats[EStat.Health] < target.stats[EStat.Health])
+                {
+                    int previousHealth = target.currentStats[EStat.Health];
+                    target.Heal(m_healthToRestore);
+                    int currentHealth = target.currentStats[EStat.Health];
+                    int diff = currentHealth - previousHealth;
+
+                    //GameManager.DialogueSystem.Main.PlayNow("You recover {0} <health>", diff);
+                    GameManager.NotificationSystem.audioPlaybackRequested.Invoke(m_drinkAudio);
+                    GameManager.InventorySystem.RemoveFromBag(this);
+                }
+                else
+                {
+                    base.Use(target, location);
+                }
+            }
+        }
+    }
+}
